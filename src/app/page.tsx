61ab39dc653f297fee169c2c0f91f8a631977741
iamcaye <cayetano.biehler@gmail.com>
2023-08-22 13:45:28 +0200

    work experience component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import CardComponent from "@/components/card-component";
 import NavBar from "@/components/nav-bar";
 import TDInfo from "@/pages/td-info";
+import WorkExperience from "@/pages/work-experience";
 
 export default function Home() {
   return (
@@ -16,22 +17,7 @@ export default function Home() {
           <p>Currently interested in <b>Web Development</b> and getting started with <b>Artificial Intelligence</b>.</p>
         </section>
 
-        <section className="pt-10 md:w-8/12 w-11/12">
-          <CardComponent title="Work Experience" languages={['python', 'typescript', 'nodejs', "c-sharp-logo", "angularjs"]} 
-            moreInfoComponent={<TDInfo/>}>
-            <div className="flex-col flex lg:flex-row items-center lg:items-top justify-between">
-              <section className="ml-2">
-                <p className="text-center md:text-left lg:text-left text-balanced">Working as a <b>Software Developer</b> at <a className="text-red-600 text-bold" rel="noopener" target="_blank" href="https://grupotopdigital.es/">Grupo Topdigital</a>.</p>
-                <ul className="ml-5 list-disc">
-                  <li>Frontend - Angular</li>
-                  <li>Backend - NET Core 6, Express.ts</li>
-                  <li>Automation Scripting & Web Scraping - Selenium, HTML Agility Pack</li>
-                </ul>
-              </section>
-              <img className="md:w-6/12 pt-3 w-8/12 sm:w-6/12 lg:w-4/12 lg:pt-0" src="https://grupotopdigital.es/wp-content/uploads/2021/07/logoH-2.png" alt="Grupo Topdgital" />
-            </div>
-          </CardComponent>
-        </section>
+        <WorkExperience />
       </main>
     </>
   )
---
+++ src/pages/work-experience.tsx
@@ -0,0 +1,23 @@
+import CardComponent from "@/components/card-component";
+import TDInfo from "./td-info";
+
+export default function WorkExperience() {
+  return (
+        <section className="pt-10 md:w-8/12 w-11/12">
+          <CardComponent title="Work Experience" languages={['python', 'typescript', 'nodejs', "c-sharp-logo", "angularjs"]} 
+            moreInfoComponent={<TDInfo/>}>
+            <div className="flex-col flex lg:flex-row items-center lg:items-top justify-between">
+              <section className="ml-2">
+                <p className="text-center md:text-left lg:text-left text-balanced">Working as a <b>Software Developer</b> at <a className="text-red-600 text-bold" rel="noopener" target="_blank" href="https://grupotopdigital.es/">Grupo Topdigital</a>.</p>
+                <ul className="ml-5 list-disc">
+                  <li>Frontend - Angular</li>
+                  <li>Backend - NET Core 6, Express.ts</li>
+                  <li>Automation Scripting & Web Scraping - Selenium, HTML Agility Pack</li>
+                </ul>
+              </section>
+              <img className="md:w-6/12 pt-3 w-8/12 sm:w-6/12 lg:w-4/12 lg:pt-0" src="https://grupotopdigital.es/wp-content/uploads/2021/07/logoH-2.png" alt="Grupo Topdgital" />
+            </div>
+          </CardComponent>
+        </section>
+  );
+}
