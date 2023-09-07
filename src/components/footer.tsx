0719c24595616f98d8b51e135514148cd56147f0
cbiehler <cayetano.biehler@topdigital.com>
2023-09-07 16:53:09 +0200

    foot mobile

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,9 @@
 export default function Footer() {
     return (
         <footer className="py-5 my-10">
-        <div className="flex justify-center align-middle">
-            <p className="text-tiny mt-1 break-normal w-4/12">© 2023 Cayetano Biehler</p>
-            <div className="flex flex-row gap-3 justify-end w-8/12 px-4">
+        <div className="block md:flex justify-center align-middle">
+            <p className="text-tiny text-center break-normal md:w-4/12 w-full my-5 md:my-1.5">© 2023 Cayetano Biehler</p>
+            <div className="flex flex-row gap-3 md:justify-end md:w-8/12 min-w-max justify-center px-4">
                 <a className="flex gap-2" href="https://github.com/iamcaye" target="_blank" rel="noopener noreferrer">
                     <i className="bi bi-github"></i>
                     <p>Github</p>
