f1c38522bd62dfbe11b3d27d69d9b43c79f09efe
iamcaye <cayetano.biehler@gmail.com>
2024-09-17 20:03:11 +0200

    update angular version td-info

--- src/pages/td-info.tsx
+++ src/pages/td-info.tsx
@@ -1,4 +1,4 @@
-export default function TDInfo () {
+export default function TDInfo() {
     return (
         <>
             <article>
@@ -9,19 +9,19 @@ export default function TDInfo () {
                     </p>
                     <p>
                         Initially, my role primarily involved learning how to use <strong>Angular 13</strong> and <strong>.NET Core 6</strong> for creating <strong>forms with validations. </strong>
-                        During this period, I also had the opportunity to learn about <strong>process automation</strong> and <strong>web scraping</strong>, which proved to be invaluable for data acquisition, 
+                        During this period, I also had the opportunity to learn about <strong>process automation</strong> and <strong>web scraping</strong>, which proved to be invaluable for data acquisition,
                         <strong>project testing</strong>, and the development of various scripts to streamline our daily tasks.
                     </p>
                     <p>
-                        Following this phase, I transitioned to a different project that had been ongoing for several years. 
-                        This project utilized older technologies, specifically <strong>.NET Framework</strong> and <strong>AngularJS</strong>. 
-                        My responsibilities included <strong>implementing new features</strong> and <strong>addressing bugs</strong> in both the frontend and backend. 
-                        This project also afforded me the opportunity to directly <strong>engage with the client</strong>, understanding their requirements and 
+                        Following this phase, I transitioned to a different project that had been ongoing for several years.
+                        This project utilized older technologies, specifically <strong>.NET Framework</strong> and <strong>AngularJS</strong>.
+                        My responsibilities included <strong>implementing new features</strong> and <strong>addressing bugs</strong> in both the frontend and backend.
+                        This project also afforded me the opportunity to directly <strong>engage with the client</strong>, understanding their requirements and
                         actively participating in decisions regarding the project's direction.
                     </p>
                     <p>
-                        Presently, I am part of a different team working on a sales tool for another client. 
-                        This project employs more up-to-date technologies, including <strong>Angular 14</strong> and <strong>Express.ts</strong>. 
+                        Presently, I am part of a different team working on a sales tool for another client.
+                        This project employs more up-to-date technologies, including <strong>Angular 17</strong> and <strong>Express.ts</strong>.
                         While this project is my current main focus, I still have ongoing tasks related to my previous projects.
                     </p>
                 </header>
