ac64ad951c63d2bdb16d724dc8baaba4da9e8141
Cayetano Biehler <cayetano.biehler@gmail.com>
2025-06-10 22:49:38 +0200

    change footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer className="py-5 my-10">
             <div className="block md:flex justify-center align-middle">
-                <p className="text-tiny text-center break-normal md:w-4/12 w-full my-5 md:my-1.5">2024 Cayetano Biehler -- i use nvim btw</p>
+                <p className="text-tiny text-center break-normal md:w-4/12 w-full my-5 md:my-1.5">2025 Cayetano Biehler -- i use nvim btw</p>
                 <div className="flex flex-row gap-3 md:justify-end md:w-8/12 min-w-max justify-center px-4">
                     <a className="flex gap-2" href="https://github.com/iamcaye" target="_blank" rel="noopener noreferrer">
                         <i className="bi bi-github"></i>
