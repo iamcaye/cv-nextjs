4a78ccfae21937b33ad8ed36ae09e89831764f7f
Cayetano Biehler <cayetano.biehler@gmail.com>
2026-06-25 09:37:06 +0200

    feat: setup dark mode, install next-themes, remove NextUI/Swiper

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,20 @@
 import type { Config } from 'tailwindcss'
-const {nextui} = require("@nextui-org/react");
 
 const config: Config = {
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: { },
+    extend: {
+      fontFamily: {
+        mono: ['JetBrains Mono', 'Fira Code', 'Consolas', 'monospace'],
+      },
+    },
   },
-  darkMode: 'class',
-  plugins: [nextui()],
+  plugins: [],
 }
+
 export default config
